a943eff94761d5b769839420d63208dd630ab084
Bhupinder Singh <khanujabhupinder09@gmail.com>
2020-06-03 18:04:10 +0530

    Added Weather App

---
+++ precache-manifest.48bd90ce7d0716ddc84409da732dead9.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5eddd260203dd0c0dfcff89e0e646326",
+    "url": "/index.html"
+  },
+  {
+    "revision": "902fa936e8262cecf288",
+    "url": "/static/css/main.c322a29a.chunk.css"
+  },
+  {
+    "revision": "00b533c10a20cdd663de",
+    "url": "/static/js/2.a46d9a0a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.a46d9a0a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "902fa936e8262cecf288",
+    "url": "/static/js/main.36a21ac4.chunk.js"
+  },
+  {
+    "revision": "4ad7be0ad1fab8ce32d0",
+    "url": "/static/js/runtime-main.7fe19d88.js"
+  },
+  {
+    "revision": "c97cb84b74a52edcb496295687e87822",
+    "url": "/static/media/cold-bg.c97cb84b.jpg"
+  },
+  {
+    "revision": "635ed87d2cc826fb213057ac40dfaff1",
+    "url": "/static/media/warm-bg.635ed87d.jpg"
+  }
+]);
